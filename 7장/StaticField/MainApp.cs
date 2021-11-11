cb4a0f8dece50bf472123500a37213a6038c0c33
gaon000 <gaon000@naver.com>
2021-11-12 01:17:16 +0900

    7장

---
+++ 7장/StaticField/MainApp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Global
+{
+    public static int Count = 0; // static으로 한정한 필드는 클래스에 소속됨
+}
+
+class ClassA
+{
+    public ClassA()
+    {
+        Global.Count++;
+    }
+}
+
+class ClassB
+{
+    public classB()
+    {
+        Global.Count++;
+    }
+}
+
+class MainApp
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine($"Global.Count : {Global.Count}"); // 0
+        new ClassA();
+        new ClassA();
+        new ClassB();
+        new ClassB();
+        Console.WriteLine($"Global.Count : {Global.Count}"); // 4
+    }
+}
