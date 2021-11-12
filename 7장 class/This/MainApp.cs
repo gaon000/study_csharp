ed11c50075f6fe437cd92bed917e1f2c759d6ddc
gaon000 <gaon000@naver.com>
2021-11-12 22:05:33 +0900

    This

---
+++ 7장 class/This/MainApp.cs
@@ -0,0 +1,48 @@
+using System;
+
+namespace This
+{
+    class Employee
+    {
+        private string Name;
+        private string Position;
+
+        public void SetName(string Name)
+        {
+            this.Name = Name; // this.Name은 Employee의 필드, Name은 매개 변수
+
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public void SetPosition(string Position)
+        {
+            this.Position = Position;
+        }
+
+        public string GetPosition()
+        {
+            return this.Position;
+        }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Employee pooh = new Employee();
+            pooh.SetName("Pooh");
+            pooh.SetPosition("Waiter");
+
+            Console.WriteLine($"{pooh.GetName()} {pooh.GetPosition()}");
+
+            Employee tigger = new Employee();
+            tigger.SetName("Tigger");
+            tigger.SetPosition("Cleaner");
+            Console.WriteLine($"{tigger.GetName()} {tigger.GetPosition()}");
+        }
+    }
+}
