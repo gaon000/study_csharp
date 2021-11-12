631192982b97b656bf31c2d59cb2544f77f4a671
gaon000 <gaon000@naver.com>
2021-11-12 22:45:41 +0900

    상속

---
+++ 7장 class/Inheritance/MainApp.cs
@@ -0,0 +1,56 @@
+using System;
+
+namespace Inheritance
+{
+    class Base // 상속하기 싫으면 sealed 한정자로 클래스를 수식하면 상속시 에러 출력됨
+    {
+        protected string Name;
+        public Base(string Name)
+        {
+            this.Name = Name;
+            Console.WriteLine($"{this.Name}.Base()");
+        }
+
+        ~Base()
+        {
+            Console.WriteLine($"{this.Name}.~Base()");
+        }
+
+        public void BaseMethod()
+        {
+            Console.WriteLine($"{Name}.BaseMethod()");
+        }
+    }
+
+    class Derived : Base // 상속
+    {
+        public Derived(string Name) : base(Name) // base는 기반 클래스를 가르킴. base()는 기반 클래스의 생성자.
+        {
+            Console.WriteLine($"{this.Name}.Derived()");
+        }
+
+        ~Derived()
+        {
+            Console.WriteLine($"{this.Name}.~Derived()");
+        }
+
+        public void DerivedMethod()
+        {
+            Console.WriteLine($"{Name}.DerivedMethod()");
+        }
+
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Base a = new Base("a");
+            a.BaseMethod();
+
+            Derived b = new Derived("b");
+            b.BaseMethod();
+            b.DerivedMethod();
+        }
+    }
+}
