7c67bd7b6bb7d60fe624e7cabeff6a856920e669
gaon000 <gaon000@naver.com>
2021-11-18 20:52:43 +0900

    프로퍼티와 생성자

---
+++ 9장 프로퍼티/ConstructorWithProperty/MainApp.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace ConstructorWithProperty
+{
+    class BirthdayInfo
+    {
+        public string Name { get; set; } // 초기화
+        public DateTime Birthday { get; set; }
+        public int Age
+        {
+            get
+            {
+                return new DateTime(DateTime.Now.Subtract(Birthday).Ticks).Year;
+            }
+        }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            BirthdayInfo birth = new BirthdayInfo()
+            {
+                Name = "서현", // 프로퍼티 생성자
+                Birthday = new DateTime(1991, 6, 28)
+            };
+
+            Console.WriteLine($"Name : {birth.Name}");
+            Console.WriteLine($"Birthday : {birth.Birthday.ToShortDateString()}");
+            Console.WriteLine($"Age : {birth.Age}");
+        }
+    }
+}
