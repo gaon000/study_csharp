fb100f3b80cbbf50215d0033347fb75613c990b3
gaon000 <gaon000@naver.com>
2021-11-18 21:08:54 +0900

    인터페이스와 프로퍼티

---
+++ 9장 프로퍼티/PropertiesInInterface/MainApp.cs
@@ -0,0 +1,63 @@
+using System;
+
+namespace PropertiesInInterface
+{
+    interface INamedValue
+    {
+        string Name
+        {
+            get;
+            set;
+        }
+
+        string Value
+        {
+            get;
+            set;
+        }
+    }
+
+    class NamedValue : INamedValue
+    {
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public string Value
+        {
+            get;
+            set;
+        }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            NamedValue name = new NamedValue()
+            {
+                Name = "이름",
+                Value = "박상현"
+            };
+
+            NamedValue height = new NamedValue()
+            {
+                Name = "키",
+                Value = "177"
+            };
+
+            NamedValue weight = new NamedValue()
+            {
+                Name = "몸무게",
+                Value = "90"
+            };
+
+            Console.WriteLine($"{name.Name} : {name.Value}");
+            Console.WriteLine($"{height.Name} : {height.Value}");
+            Console.WriteLine($"{weight.Name} : {weight.Value}");
+
+        }
+    }
+}
