6a713754b6c01582dc058f94e5cd879475135203
gaon000 <gaon000@naver.com>
2021-12-01 23:10:11 +0900

    Trace

---
+++ 12장 예외/StackTrace/MainApp.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace StackTrace
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int a = 1;
+                Console.WriteLine(3 / --a);
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e.StackTrace); // 문제가 발생한 부분의 위치를 알려줌
+            }
+        }
+    }
+}
